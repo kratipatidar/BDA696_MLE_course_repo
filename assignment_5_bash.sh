1e7cb93e62c5bd8d3b68276264ea13085d3f1aa6
kratipatidar <55113076+kratipatidar@users.noreply.github.com>
2020-11-29 09:56:40 -0500

    Update assignment_5_bash.sh

--- assignment_5_bash.sh
+++ assignment_5_bash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS baseball"
+mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS baseball;"
 mysql -u root -p baseball < baseball.sql
-mysql -u root -p < assignment_5_sql.sql
+mysql -u root -p baseball < assignment_5_sql.sql
